a5e11dd8da63c9873a8dda51fe2bbeb0fb990af7
WatchingAnime <48404900+UncleSalad@users.noreply.github.com>
2019-03-23 19:34:28 +0300

    Add files via upload

---
+++ SaladStepper-library/SaladStepper.cpp
@@ -0,0 +1,53 @@
+﻿#include "SaladStepper.h"
+#include "Arduino.h"
+SaladStepper::SaladStepper(int in1, int in2, int in3, int in4) {
+	_in1 = in1;
+	_in2 = in2;
+	_in3 = in3;
+	_in4 = in4;
+}
+
+void SaladStepper::begin() {
+	pinMode(_in1, OUTPUT);
+	pinMode(_in2, OUTPUT);
+	pinMode(_in3, OUTPUT);
+	pinMode(_in4, OUTPUT);
+}
+
+void SaladStepper::stepLeft(int mode) {
+	if (mode) {
+		_fullStepCounter++;
+		if (_fullStepCounter >= 4) _fullStepCounter = 0;
+		digitalWrite(_in1, _fullStepMap[_fullStepCounter][0]);
+		digitalWrite(_in2, _fullStepMap[_fullStepCounter][1]);
+		digitalWrite(_in3, _fullStepMap[_fullStepCounter][2]);
+		digitalWrite(_in4, _fullStepMap[_fullStepCounter][3]);
+	}
+	else {
+		_halfStepCounter++;
+		if (_halfStepCounter >= 8) _halfStepCounter = 0;
+		digitalWrite(_in1, _halfStepMap[_halfStepCounter][0]);
+		digitalWrite(_in2, _halfStepMap[_halfStepCounter][1]);
+		digitalWrite(_in3, _halfStepMap[_halfStepCounter][2]);
+		digitalWrite(_in4, _halfStepMap[_halfStepCounter][3]);
+	}
+}
+
+void SaladStepper::stepRight(int mode) {
+	if (mode) {
+		_halfStepCounter--;
+		if (_halfStepCounter <= -1) _halfStepCounter = 7;
+		digitalWrite(_in1, _fullStepMap[_fullStepCounter][0]);
+		digitalWrite(_in2, _fullStepMap[_fullStepCounter][1]);
+		digitalWrite(_in3, _fullStepMap[_fullStepCounter][2]);
+		digitalWrite(_in4, _fullStepMap[_fullStepCounter][3]);
+	}
+	else {
+		_halfStepCounter--;
+		if (_halfStepCounter <= -1) _halfStepCounter = 7;
+		digitalWrite(_in1, _halfStepMap[_halfStepCounter][0]);
+		digitalWrite(_in2, _halfStepMap[_halfStepCounter][1]);
+		digitalWrite(_in3, _halfStepMap[_halfStepCounter][2]);
+		digitalWrite(_in4, _halfStepMap[_halfStepCounter][3]);
+	}
+}
---
+++ SaladStepper-library/SaladStepper.h
@@ -0,0 +1,32 @@
+﻿#ifndef _SaladStepper_h
+#define _SaladStepper_h
+#include "Arduino.h"
+#define HALFSTEP 0
+#define FULLSTEP  1
+	class SaladStepper {
+	public:
+		SaladStepper(int in1, int in2, int in3, int in4);
+		void begin();
+		void stepLeft(int mode);
+		void stepRight(int mode);
+	private:
+		int _in1, _in2, _in3, _in4, _fullStepCounter, _halfStepCounter;
+		int _fullStepMap[4][4] = {
+			{1, 1, 0, 0},
+			{0, 1, 1, 0},
+			{0, 0, 1, 1},
+			{1, 0, 0, 1},
+		};
+		int _halfStepMap[8][4] = {
+			{1, 0, 0, 0},
+			{1, 1, 0, 0},
+			{0, 1, 0, 0},
+			{0, 1, 1, 0},
+			{0, 0, 1, 0},
+			{0, 0, 1, 1},
+			{0, 0, 0, 1},
+			{1, 0, 0, 1}
+		};
+	};
+#endif
+
